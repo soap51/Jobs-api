6d888e54e3a430d3fc1311c1ac89c6c09ea286dd
Poramet Thawinkarn <soap51@outlook.com>
2019-12-28 23:56:00 +0700

    use regex to parse html

--- scrappy.py
+++ scrappy.py
@@ -3,33 +3,42 @@ from selenium import webdriver
 import requests
 import bs4
 from bs4 import BeautifulSoup
+import deepcut
+import os
+import re
+import pandas as pd
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 driver = webdriver.Chrome('./chromedriver.exe')  # Optional argument, if not specified will search path.
-driver.get('https://th.jobsdb.com/th')
-time.sleep(4) # Let the user actually see something!
+driver.get('https://th.jobsdb.com/th/th')
+time.sleep(0) # Let the user actually see something!
 driver.find_element_by_id('SecondaryLanguageButton').click()
 search_text = driver.find_element_by_name('keywordInput')
-search_text.send_keys('data scientist')
+keyword = "data scientist"
+search_text.send_keys(keyword)
 driver.find_element_by_id('searchbox-submit').click()
 position_links = driver.find_elements_by_class_name("posLink")
+result = []
 for link in position_links:
+   
     url = link.get_attribute("href")
     data = requests.get(url).text
-    soup = BeautifulSoup(data ,features="html.parser")
+    soup = BeautifulSoup(data ,features="html.parser")    
+    time.sleep(5) 
     div = soup.find_all("div", class_="jobad-primary-details")[0]
     span = div.find("span")     
-    for ul in span.find_all("ul"):
-        for li in ul.find("li"):               
-            try:                      
-                if isinstance(li, type(None)) and type(li) == bs4.element.NavigableString or type(li) == bs4.element.Tag:
-                    print(li)
-                    if len(li.string) != 1:
-                        
-                        print(li.content)
-                pass
-            except (AttributeError, TypeError) as e:
-                print("Ignores ",e)
-                pass
-       
+    cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+    detail = re.sub(cleanr, '', str(div))                 
+    detail = detail.replace("\r\n", "")
+    detail = detail.replace("\n", "")
+    detail = detail.replace(u'\xa0', u' ')
+    #print(detail)
+    result.append({"keyword": keyword , "detail" : detail})
+    print("----")    
+    text_token = deepcut.tokenize(detail)
+    print(text_token)
+    print("----")
+   
   
 time.sleep(5) # Let the user actually see something!
 driver.quit()
