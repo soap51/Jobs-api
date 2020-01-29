ee51942f650b0819cfc74099998c06ae98dc12ab
Poramet Thawinkarn <soap51@outlook.com>
2020-01-30 01:30:48 +0700

    Add Lemma

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ import spacy
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
 from spacy.lang.en.stop_words import STOP_WORDS
+from spacy.lemmatizer import Lemmatizer
+from spacy.lookups import Lookups
 import time
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     nlp =  English()
+    lookups = Lookups()
+    lookups.add_table("lemma_rules", {"noun": [["s", ""]]})
+    lemmatizer = Lemmatizer(lookups)
     # Create a blank Tokenizer with just the English vocab
     #nlp.tokenizer = Tokenizer(nlp.vocab)
-    source = requests.get("https://th.jobsdb.com/TH/TH/Search/FindJobs?KeyOpt=COMPLEX&JSRV=1&RLRSF=1&JobCat=131&JSSRC=HPSS").text
+    source = requests.get("https://th.jobsdb.com/TH/TH/Search/FindJobs?KeyOpt=COMPLEX&JSRV=1&RLRSF=1&JobCat=1&SearchFields=Positions,Companies&Key=data%20engineer&JSSRC=HPSS").text
     soup = BeautifulSoup(source ,features="html.parser")  
     all_a_tag = soup.find_all("a",class_="posLink" )   
     count_dict= {}
@@ -27,28 +32,19 @@ def hello_world():
         plain_text_jobs_detail = re.sub(clean_re, '',  str(div_jobs_detail))       
         plain_text_jobs_detail = " ".join(plain_text_jobs_detail.split())
         text_token = nlp(plain_text_jobs_detail)
+        
         for w in text_token:
             lexme = nlp.vocab[w.text]
-            if lexme.is_stop == False:         
-                if w.text in count_dict and not w.text.isalnum() :                               
-                    if w.text == "Database" or w.text == "databases" or w.text == "database": 
-                        print(w.text)                       
-                        word = "database"         
-                    else :
-                        word = w.text.lower()      
-                    count_dict[word] += 1
-                    print(count_dict)
-                    time.sleep(4)
-                else:                 
-                    if w.text == "Database" or w.text == "databases" or w.text == "database":
-                        print(w.text)                       
-                        word = "database"                              
-                    else :
-                        word = w.text.lower()
-                            
-                    count_dict[word] = 1                                             
-                    print(count_dict)
-                    time.sleep(4)
+            if lexme.is_stop == False and w.text != "(" and w.text != ")" and w.text != ":" and w.text != "-"  :                                           
+                word = w.lemma_.lower()                                  
+                word = lemmatizer(word, "NOUN")[0]
+                
+                if (word in count_dict):                               
+                    count_dict[word] += 1             
+                else:                                   
+                    count_dict[word] = 1    
+             
+                #print(word + " " +str(count_dict[word]))
                 
         
     return count_dict
