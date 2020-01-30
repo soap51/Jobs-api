f2f21fc0336117cfd6e32d6e7f2817d8a5440ffe
Poramet Thawinkarn <soap51@outlook.com>
2020-01-31 03:37:46 +0700

    Add Change Page

--- app.py
+++ app.py
@@ -6,45 +6,43 @@ import spacy
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
 from spacy.lang.en.stop_words import STOP_WORDS
-from spacy.lemmatizer import Lemmatizer
-from spacy.lookups import Lookups
+
 import time
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/<string:occupation>')
+def getData(occupation):
     nlp =  English()
-    lookups = Lookups()
-    lookups.add_table("lemma_rules", {"noun": [["s", ""]]})
-    lemmatizer = Lemmatizer(lookups)
-    # Create a blank Tokenizer with just the English vocab
-    #nlp.tokenizer = Tokenizer(nlp.vocab)
-    source = requests.get("https://th.jobsdb.com/TH/TH/Search/FindJobs?KeyOpt=COMPLEX&JSRV=1&RLRSF=1&JobCat=1&SearchFields=Positions,Companies&Key=data%20engineer&JSSRC=HPSS").text
+    source = requests.get("https://th.jobsdb.com/TH/TH/Search/FindJobs?KeyOpt=COMPLEX&JSRV=1&RLRSF=1&JobCat=1&SearchFields=Positions,Companies&Key="+occupation+"&JSSRC=HPSS").text
     soup = BeautifulSoup(source ,features="html.parser")  
-    all_a_tag = soup.find_all("a",class_="posLink" )   
+    links = []
+    poslink = soup.find_all("a",class_="posLink" )
+    link_next_page = soup.find("a",class_="pagebox-next" )
+    posLinks = []
+    posLinks.extend(poslink)    
+    while( not((link_next_page) is None) and link_next_page.get("href") != ""):      
+        links.append(link_next_page.get("href"))
+        temp_source = requests.get(link_next_page.get("href")).text
+        temp_soup = BeautifulSoup(temp_source ,features="html.parser")
+        link_next_page = temp_soup.find("a",class_="pagebox-next" ) 
+        poslink = temp_soup.find_all("a",class_="posLink" ) 
+        posLinks.extend(poslink)          
     count_dict= {}
     word = ""
-    for a in all_a_tag:
+    for a in posLinks:
         html_jobs = requests.get(a["href"]).text
         soup_jobs = BeautifulSoup(html_jobs ,features="html.parser")  
         div_jobs_detail = soup_jobs.find("div", class_="jobad-primary-details")
         clean_re = re.compile(' {2, }|<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')  
         plain_text_jobs_detail = re.sub(clean_re, '',  str(div_jobs_detail))       
         plain_text_jobs_detail = " ".join(plain_text_jobs_detail.split())
-        text_token = nlp(plain_text_jobs_detail)
-        
+        text_token = nlp(plain_text_jobs_detail)        
         for w in text_token:
             lexme = nlp.vocab[w.text]
             if lexme.is_stop == False and w.lemma_.isalpha() :                                           
                 word = w.lemma_.lower()                                  
-                word = lemmatizer(word, "NOUN")[0]
-                
                 if (word in count_dict):                               
                     count_dict[word] += 1             
                 else:                                   
-                    count_dict[word] = 1    
-             
-                #print(word + " " +str(count_dict[word]))
-                
-        
+                    count_dict[word] = 1                                         
     return count_dict
