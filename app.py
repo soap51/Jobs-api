2361fa7ac2831cb6a1fed68296aaae03dd6997e7
Poramet Thawinkarn <soap51@outlook.com>
2020-02-10 20:33:04 +0700

    Complete

--- app.py
+++ app.py
@@ -8,160 +8,182 @@ import pandas as pd
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
 from spacy.lang.en.stop_words import STOP_WORDS
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from spacy.tokens import Doc
+from spacy.vocab import Vocab
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 class CustomTokenizer(object):
-       def __init__(self, vocab):
+    def __init__(self, vocab):
         self.vocab = vocab
 
+    def __call__(self, text):
+        new_vocabs = [
+            " R ",
+            "R, ",
+            ", R",
+            "Hadoop",
+            "Big Data",
+            "Big data",
+            "big data",
+            "Data",
+            "data",
+            "Business",
+            "Machine Learning",
+            "machine learning",
+            "Deep Learning",
+            "deep learning",
+            "Neural Network",
+            "neural network",
+            "Neural Networks",
+            "neural networks",       
+            "Computer Vision",
+            "Data Mining", 
+            "data mining",
+            "Text Mining", 
+            "ERP",
+            "text mining", 
+            "Tableau", 
+            "tableau",     
+            "Data Scientist",
+            "Python",
+            "Javascript",
+            "PowerBI", 
+            "Power BI", 
+            "power bi", 
+            "SQL",
+            "Sql",
+            "sql",
+            "Data Studio", 
+            "Google Analytics", 
+            "Amazon Web Services", 
+            "AWS", 
+            "Amazon Web Service",
+            "MongoDB", 
+            "NoSQL",
+            "Nosql",
+            "nosql",
+            "NOSQL",  
+            "hive",
+            "Hive",
+            "HIVE",
+            "apache hive",
+            "Apache Hive",
+            "Apache Pig",
+            "Pig",
+            "PIG",
+            "Spark",
+            "Apache Spark",
+            "Apache spark",
+            "apache spark",
+            "HDFS",
+            "Hadoop Distributed File System",
+            "Hbase",
+            "Sqoop",
+            "ZooKeeper",
+            "Apache ZooKeeper",
+            "Apache Airflow",
+            "NOSQL",
+            "MapReduce",
+            "Solr",
+            "Lucene",
+            "C++",
+            "Artificial Intelligence",
+            "AI",
+            "Expert Systems",
+            "Scala",
+            "Java",
+            "Natural Language Processing",
+            "natural language processing",
+            "Pattern Recognition",
+            "pattern recognition",
+            "Recommendation Systems",
+            "recommendation systems",
+            "BigQuery",
+            "Cassandra",
+            "ElasticSearch",
+            "Kafka",
+            "Flask",
+            "Caffe",
+            "Torch",
+            "Scikit-Learn",
+            "Theano",
+            "MLlib",
+            "Classification",
+            "Regression",
+            "Clustering",
+            "Association Rules",
+            "Support Vector Machines",
+            "support vector machines",
+            "Storm",
+            "Azkaban",
+            "Luigi",
+            "Tensorflow",
+            "Keras",
+            "Pytorch",
+            "BigQuery",
+            "Dataproc",
+            "ML Engine",
+            "docker",
+            "kubernetes",
+            "Google Cloud",
+            "SAS",
+            "Logistic Regression",
+            "KNN",
+            "k-Nearest Neighbor",
+            "MXNet",
+            "pandas",
+            "information retrieval",
+            "Information Retrieval",
+            "data visualization",
+            "nodejs",
+            "Nodejs",
+            "Node.js",
+            "node.js",
+            "python",
+            "Python",
+            "Redis",
+            "Golang",
+            "Java",
+            "C++",
+            "C#",
+            "J2EE",
+            "JSP",
+            "Servlet",
+            "JQuery",
+            "JSF",
+            "Hibernate",
+            "JDBC",
+            "Apache tomcat",
+            "Maven",
+            "Oracle database",
+            "Agile",
+            "SCRUM",
+            "REST API",
+            "Rest Api",
+            "react",
+            "HTML",
+            "html",
+            "Css",
+            "CSS",
+            "Angular",
+            "Vue"
+        ]
+        words = text.split(' ')
+        empty_doc = Doc(self.vocab ,words=words)       
+        for word in new_vocabs:    
+            empty_doc.vocab.strings.add(word)
+   
+        return empty_doc
+
+
 @app.route('/<string:occupation>' , methods=["GET"])
+@cross_origin()
 def getData(occupation):
     nlp =  English()
-    words = [
-        " R ",
-        "R, ",
-        ", R",
-        "Hadoop",
-        "Big Data",
-        "Big data",
-        "big data",
-        "Data",
-        "data",
-        "Business",
-        "Machine Learning",
-        "machine learning",
-        "Deep Learning",
-        "deep learning",
-        "Neural Network",
-        "neural network",
-        "Neural Networks",
-        "neural networks",       
-        "Computer Vision",
-        "Data Mining", 
-        "data mining",
-        "Text Mining", 
-        "ERP",
-        "text mining", 
-        "Tableau", 
-        "tableau",     
-        "Data Scientist",
-        "Python",
-        "Javascript",
-        "PowerBI", 
-        "Power BI", 
-        "power bi", 
-        "SQL",
-        "Sql",
-        "sql",
-        "Data Studio", 
-        "Google Analytics", 
-        "Amazon Web Services", 
-        "AWS", 
-        "Amazon Web Service",
-        "MongoDB", 
-        "NoSQL",
-        "Nosql",
-        "nosql",
-        "NOSQL",  
-        "hive",
-        "Hive",
-        "HIVE",
-        "apache hive",
-        "Apache Hive",
-        "Apache Pig",
-        "Pig",
-        "PIG",
-        "Spark",
-        "Apache Spark",
-        "Apache spark",
-        "apache spark",
-        "HDFS",
-        "Hadoop Distributed File System",
-        "Hbase",
-        "Sqoop",
-        "ZooKeeper",
-        "Apache ZooKeeper",
-        "Apache Airflow",
-        "NOSQL",
-        "MapReduce",
-        "Solr",
-        "Lucene",
-        "C++",
-        "Artificial Intelligence",
-        "AI",
-        "Expert Systems",
-        "Scala",
-        "Java",
-        "Natural Language Processing",
-        "natural language processing",
-        "Pattern Recognition",
-        "pattern recognition",
-        "Recommendation Systems",
-        "recommendation systems",
-        "BigQuery",
-        "Cassandra",
-        "ElasticSearch",
-        "Kafka",
-        "Flask",
-        "Caffe",
-        "Torch",
-        "Scikit-Learn",
-        "Theano",
-        "MLlib",
-        "Classification",
-        "Regression",
-        "Clustering",
-        "Association Rules",
-        "Support Vector Machines",
-        "support vector machines",
-        "Storm",
-        "Azkaban",
-        "Luigi",
-        "Tensorflow",
-        "Keras",
-        "Pytorch",
-        "BigQuery",
-        "Dataproc",
-        "ML Engine",
-        "Google Cloud",
-        "SAS",
-        "Logistic Regression",
-        "KNN",
-        "k-Nearest Neighbor",
-        "MXNet",
-        "pandas",
-        "information retrieval",
-        "Information Retrieval",
-        "data visualization",
-        "nodejs",
-        "Nodejs",
-        "Node.js",
-        "node.js",
-        "python",
-        "Python",
-        "Redis",
-        "Golang",
-        "Java",
-        "C++",
-        "C#",
-        "J2EE",
-        "JSP",
-        "Servlet",
-        "JQuery",
-        "JSF",
-        "Hibernate",
-        "JDBC",
-        "Apache tomcat",
-        "Maven",
-        "Oracle database",
-        "Agile",
-        "SCRUM",
-        "REST API",
-        "Rest Api"
-    ]
+   
   
     source = requests.get("https://th.jobsdb.com/TH/TH/Search/FindJobs?KeyOpt=COMPLEX&JSRV=1&RLRSF=1&JobCat=1&SearchFields=Positions,Companies&Key="+occupation+"&JSSRC=HPSS").text
     soup = BeautifulSoup(source ,features="html.parser")  
@@ -180,7 +202,7 @@ def getData(occupation):
     
     count_dict= {}
     word = ""
-    
+    nlp.tokenizer = CustomTokenizer(nlp.vocab)
     for a in posLinks:
         html_jobs = requests.get(a["href"]).text
         soup_jobs = BeautifulSoup(html_jobs ,features="html.parser")  
@@ -188,8 +210,12 @@ def getData(occupation):
         clean_re = re.compile(' {2, }|<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')  
         plain_text_jobs_detail = re.sub(clean_re, '',  str(div_jobs_detail))       
         plain_text_jobs_detail = " ".join(plain_text_jobs_detail.split())
-        text_token = Doc(nlp.vocab, words=words)    
+        
+       
+ 
+        text_token = nlp(plain_text_jobs_detail) 
         for w in text_token:
+            print(w)
             lexme = nlp.vocab[w.text]
             if lexme.is_stop == False and w.lemma_.isalpha() :                                           
                 word = w.lemma_.lower()                                  
@@ -201,10 +227,11 @@ def getData(occupation):
     
     df = pd.DataFrame.from_dict(count_dict, columns=["count"], orient="index")
     
-    return jsonify((df.sort_values(["count"], ascending=False).head(8).to_json() ))
+    return jsonify((df.sort_values(["count"], ascending=False).to_json() ))
 
 # A welcome message to test our server
 @app.route('/')
+@cross_origin()
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
